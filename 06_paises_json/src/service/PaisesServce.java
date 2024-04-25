9bffb397210cfcb50787f903f05cd9ac7b2f35b9
manana <manana@A13E21>
2024-04-25 13:41:30 +0200

    22

--- 06_paises_json/src/service/PaisesServce.java
+++ 06_paises_json/src/service/PaisesServce.java
@@ -1,5 +1,67 @@
 package service;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
+import model.Pais;
+
 public class PaisesServce {
+	/*Implementar lógica de negocio de una aplicación que proporcione información
+	sobre paises. Cada pais se caracteriza por: nombre,continente,capital,habitantes
+
+	-Lista de continentes
+	-Lista de paises a partir del continente
+	-Pais más poblado
+	-Tabla con paises agurpados por continente
+	-Pais a partir de su capital */
+	
+String ruta="C:\\Users\\manana\\Desktop\\BELENGB\\JSON\\paises.json";
+	
+//**************************************************************************************
+	private Stream <Pais> getPaises() {
+		try {
+			Gson gson=new Gson();
+			return Arrays.stream(gson.fromJson(new FileReader(ruta), Pais[].class)); //Array<stream>
+			
+		}catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Stream.empty();
+		}
+	}
+	
+//*********************************************************************************************
+	public List<String> listaContinentes () {
+		return getPaises() //Stream<Pais>
+				.map(n->n.getContinente())
+				.distinct()
+				.toList();
+		
+	}
+	
+//*************************************************************************************
+	public List<Pais> paisContiente (String continente) {
+		return getPaises()
+				.filter(e->e.getContinente().equals(continente))
+				.toList();	
+	}
+	
+//***********************************************************************************
+	public String paisMasPoblado() {
+		return getPaises()
+				.max(Comparator.comparingInt(n->n.getHabitantes()))
+				.toString();
+	}
+	
+//*****************************************************************************************
+	public 
 
 }
