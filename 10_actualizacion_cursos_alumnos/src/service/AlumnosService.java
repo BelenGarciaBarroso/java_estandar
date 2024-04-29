691b5b5a43a2bef7a57380508260495030f1596d
manana <manana@A13E21>
2024-04-29 13:53:29 +0200

    29/04

--- 10_actualizacion_cursos_alumnos/src/service/AlumnosService.java
+++ 10_actualizacion_cursos_alumnos/src/service/AlumnosService.java
@@ -2,4 +2,9 @@ package service;
 
 public class AlumnosService {
 
+	String cadenaConexion="jdbc:mysql://localhost:3306/alumnos";
+	String usuario="root";
+	String password="root";
+	
+	private Alumno 
 }
