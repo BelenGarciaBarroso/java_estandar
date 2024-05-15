2b08686e0ad3d70e692531e55a9b636d4e2dda7f
manana <manana@A13E21>
2024-05-15 11:30:03 +0200

    15/05

---
+++ 43_paises_SWING_COMBO/src/graficos/VentanaPaises.java
@@ -0,0 +1,71 @@
+package graficos;
+
+import java.awt.EventQueue;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import adaptadores.ComboBoxModelContinentesImpl;
+import service.PaisesServiceFactory;
+
+public class VentanaPaises extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPaises frame = new VentanaPaises();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaPaises() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Continentes:");
+		lblNewLabel.setBounds(30, 27, 81, 14);
+		contentPane.add(lblNewLabel);
+		
+		JComboBox<String> comboContinentes = new JComboBox();
+		comboContinentes.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				String seleccionado=(String)comboContinentes.getSelectedItem();
+				var service= PaisesServiceFactory.getPaisesService();
+				JOptionPane.showMessageDialog(VentanaPaises.this, "Paises de "+seleccionado+" :"+service.getPaisesFiltradosPor(seleccionado).size());
+				
+			}
+		});
+		comboContinentes.setBounds(97, 23, 117, 18);
+		contentPane.add(comboContinentes);
+		comboContinentes.setModel(new ComboBoxModelContinentesImpl());
+		
+			
+		
+		
+	}
+}
